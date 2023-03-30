9b9043f0231d02e22b8eaa07ad21746f29cda76a
pranjalisakhale <pranjalimakh@gmail.com>
2023-03-30 14:18:02 +0530

    team

---
+++ src/test/java/Com/SecureHome/main/HomeLoanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.SecureHome.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLoanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
